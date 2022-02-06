de59adaadbc4d619d53d166338faf878e19eb66b
Bozena <bokopec@student.agh.edu.pl>
2022-02-06 11:09:09 +0100

    dodanie funkcji rysujacej wisielca

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -57,3 +57,22 @@ public class Hangman {
     public static void main(String[] args) {
     }
 }
+
+
+
+public static String buildWord(String[] letters){
+
+        String word = "";
+
+        int index = letters.length;
+
+        for (int i = 0; i<index;i++){
+        if (letters[i] == "_"){
+        word =  word + " ";
+        }
+        word = word + letters[i];
+        }
+
+        return word;
+        }
+        }

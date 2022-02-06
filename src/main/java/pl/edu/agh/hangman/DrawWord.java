439cdbed9b924b26f0a34c78219b8efa98ff3ddf
jacobitedge <wojciechkaras14@gmail.com>
2022-02-06 12:02:58 +0100

    Draw Word

--- src/main/java/pl/edu/agh/hangman/DrawWord.java
+++ src/main/java/pl/edu/agh/hangman/DrawWord.java
@@ -1,2 +1,36 @@
-package pl.edu.agh.hangman;public class DrawWord {
+package pl.edu.agh.hangman;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DrawWord {
+
+    private String selectedWord;
+
+    public DrawWord() {
+        selectedWord = new String();
+        this.selectedWord = drawWord();
+    }
+
+    public String getSelectedWord() {
+        return selectedWord;
+    }
+
+    public void setSelectedWord(String selectedWord) {
+        this.selectedWord = selectedWord;
+    }
+
+    private static String drawWord() {
+        Random random = new Random();
+
+        ReadFile readWords = new ReadFile();
+        List<String> wordsList = new ArrayList<String>();
+        wordsList = readWords.getWordsList();
+
+        int randomNumber = random.nextInt(wordsList.size());
+        String randomWord = wordsList.get(randomNumber);
+
+        return randomWord;
+    }
 }

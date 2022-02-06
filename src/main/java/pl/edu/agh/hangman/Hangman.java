50872c520aa6b3a4ed00a56cf50a2c623046cd4d
Anna G <gumulka@student.agh.edu.pl>
2022-02-06 10:08:06 +0100

    wyswietlanie slowa

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,12 @@
 package pl.edu.agh.hangman;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -53,7 +60,24 @@ public class Hangman {
                     "      |\n" +
                     "========"
     };
+    public static List<String> words= new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //readFile("slowa.txt");
+
+
     }
+
+
+    public static String drawWord(String word, String userWord, char x) {
+        for (int i=0;i<word.length(); i++){
+            if(word[i]==x) userWord[i]=x;
+            else {
+                userWord[i]='-';
+            }
+        }
+        return userWord;
+
+    }
+
 }
